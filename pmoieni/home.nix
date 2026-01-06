43aae9aed99cb9af9d131c29ec221daebe7a96c9
pmoieni <62774242+pmoieni@users.noreply.github.com>
2026-01-06 19:54:47 +0330

    fix

--- pmoieni/home.nix
+++ pmoieni/home.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ config, inputs, ... }:
 {
   imports = [
     inputs.stylix.homeModules.stylix
@@ -17,6 +17,37 @@
     };
 
     stateVersion = "25.05";
+  };
+
+  home.file = {
+    "${config.xdg.configHome}/alacritty" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dots/config/alacritty";
+      recursive = true;
+    };
+    "${config.xdg.configHome}/nvim" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dots/config/nvim";
+      recursive = true;
+    };
+    "${config.xdg.configHome}/nvim-dev" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dots/config/nvim-dev";
+      recursive = true;
+    };
+    "${config.xdg.configHome}/zed" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dots/config/zed";
+      recursive = true;
+    };
+    "${config.xdg.configHome}/fish" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dots/config/fish";
+      recursive = true;
+    };
+    "${config.xdg.configHome}/niri" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dots/config/niri";
+      recursive = true;
+    };
+    "${config.xdg.configHome}/noctalia" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dots/config/noctalia";
+      recursive = true;
+    };
   };
 
   programs.home-manager.enable = true;
