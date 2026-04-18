133840e65dedeea013e9dd4e8e3ab41faa22975d
pmoieni <62774242+pmoieni@users.noreply.github.com>
2026-04-18 16:32:46 +0330

    update

--- hm/pmoieni/gui/default.nix
+++ hm/pmoieni/gui/default.nix
@@ -2,10 +2,20 @@
   lib,
   isNixOS,
   config,
+  pkgs,
   ...
 }:
 {
   imports = [ ] ++ lib.optionals isNixOS [ ./stylix.nix ];
 
   home.packages = lib.mkIf config.system.gui.enable ([ ]);
+
+  xdg.dataFile = {
+    # v2rayn
+    "v2rayN/bin/sing_box/sing-box".source = "${pkgs.sing-box}/bin/sing-box";
+    "v2rayN/bin/xray/xray".source = "${pkgs.xray}/bin/xray";
+    "v2rayN/bin/geoip.dat".source = "${pkgs.v2ray-geoip}/share/v2ray/geoip.dat";
+    "v2rayN/bin/geosite.dat".source = "${pkgs.v2ray-domain-list-community}/share/v2ray/geosite.dat";
+
+  };
 }
