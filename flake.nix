f809ec2bdf88bade0078bff602a49065a47eecfe
pmoieni <62774242+pmoieni@users.noreply.github.com>
2026-06-22 12:45:41 +0330

    stylix

--- flake.nix
+++ flake.nix
@@ -13,6 +13,11 @@
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    stylix = {
+      url = "github:nix-community/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
